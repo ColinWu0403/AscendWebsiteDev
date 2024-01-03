20ad8b17d40ef5d1b4ccef950393598501f30a19
colin wu <colinwu0403@gmail.com>
2024-01-02 21:06:00 -0600

    Added Join Component

---
+++ src/components/Join.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Join = () => {
+  return (
+    <div className="bg-[#fff] py-16 text-[#686768]">
+      <div className="container mx-auto max-w-full">
+        <div className="text-center mt-8 mb-4">
+          <h1 className="text-[#f09400] text-4xl font-medium mb-4 relative inline-block">
+            Join Ascend
+            <span className="block w-12 h-1 bg-[#f09400] mx-auto mt-2 font-bold mb-4"></span>
+          </h1>
+        </div>
+
+        {/* Add join button + other social media link */}
+      </div>
+    </div>
+  );
+};
+
+export default Join;
